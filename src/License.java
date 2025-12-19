5d79b11e7aa1030e0e484488f19e3a63096a428a
Sourabh Jain <sourabh.jain@technoduxx.com>
2025-12-19 17:33:19 +0530

    License Program

---
+++ src/License.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class License {
+    public static void main(String[] args)
+    {
+        // int a = 18;
+        Scanner user=new Scanner(System.in);
+        System.out.println("Enter the Age");
+        int license = user.nextInt();
+        if (license>=18)
+        {
+           System.out.println("Eligible for License");
+        }
+        else
+        {
+            System.out.println("Not Eligible");
+        }
+    }
+}
